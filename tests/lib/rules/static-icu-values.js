fe8951a4f5473afd15e5a4b381947a35503b1907
Leonardo Dino <leonardo@kunstdesign.com.br>
2020-03-02 20:49:08 +0000

    :sparkles: add static-icu-values rule

---
+++ tests/lib/rules/static-icu-values.js
@@ -0,0 +1,55 @@
+const test = require('../../../lib/test-utils')
+const rule = require('../../../lib/rules/static-icu-values')
+
+const locale = { greeting: 'hello', hello: 'hello {name}' }
+
+test(locale)(rule, {
+  valid: [
+    '(<SomeComponent id="something" defaultMessage="{" />)',
+    'someFunctionCall({key: {id: "something", defaultMessage: "{"}})',
+    '(<FormattedMessage id="greeting" defaultMessage="hello" />)',
+    'formatMessage({id: "greeting", defaultMessage: "hello"})',
+    'formatMessage({id: "greeting", defaultMessage: "hello {name}"}, {name})',
+    'formatMessage({id: "greeting", defaultMessage: `hello {name}`}, {name})',
+    '(<FormattedMessage id="hello" defaultMessage="hello {name}" values={{ "name": name }} />)',
+    '(<FormattedMessage id="hello" defaultMessage={`hello {name}`} values={{name}} />)',
+    'defineMessages({key: {id: "greeting", defaultMessage: "hello"}})',
+    'defineMessages({key: {id: "greeting", defaultMessage: "hello {name}"}})',
+    'defineMessages({key: {id: "greeting", defaultMessage: `hello {name}`}})',
+  ],
+  invalid: [
+    {
+      code: '(<FormattedMessage id="hello" defaultMessage="hello {name}" />)',
+      errors: [/"name"/],
+    },
+    {
+      code:
+        '(<FormattedMessage id="hello" defaultMessage="hello {name}" values={{}} />)',
+      errors: [/"name"/],
+    },
+    {
+      code:
+        '(<FormattedMessage id="hello" defaultMessage={`hello {nome}`} values={{name}} />)',
+      errors: [/"nome"/],
+    },
+    {
+      code:
+        'formatMessage({id: "greeting", defaultMessage: "hello {name}"}, {})',
+      errors: [/"name"/],
+    },
+    {
+      code:
+        'formatMessage({id: "greeting", defaultMessage: `hello {nome}`}, {name})',
+      errors: [/"nome"/],
+    },
+    {
+      code: 'formatMessage({id: "plural", defaultMessage: "{count, plural, =0 {some {variable}} other {# objects}}"})',
+      errors: [/"count", "variable"/],
+    },
+    {
+      code:
+        'defineMessages({key: {id: "greeting", defaultMessage: "hello {"}})',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+  ],
+})
---
+++ tests/lib/rules/validate-icu-syntax.js
@@ -0,0 +1,66 @@
+const test = require('../../../lib/test-utils')
+const rule = require('../../../lib/rules/validate-icu-syntax')
+
+const locale = { greeting: 'hello', hello: 'hello {name}' }
+
+test(locale)(rule, {
+  valid: [
+    '(<SomeComponent id="something" defaultMessage="{" />)',
+    'someFunctionCall({key: {id: "something", defaultMessage: "{"}})',
+    '(<FormattedMessage id="greeting" defaultMessage="hello" />)',
+    'formatMessage({id: "greeting", defaultMessage: "hello"})',
+    'formatMessage({id: "greeting", defaultMessage: "hello {name}"})',
+    'formatMessage({id: "greeting", defaultMessage: `hello {name}`})',
+    '(<FormattedMessage id="hello" defaultMessage="hello {name}" values={{name}} />)',
+    'defineMessages({key: {id: "greeting", defaultMessage: "hello"}})',
+    'defineMessages({key: {id: "greeting", defaultMessage: "hello {name}"}})',
+    'defineMessages({key: {id: "greeting", defaultMessage: `hello {name}`}})',
+
+    'formatMessage()',
+
+    '(<FormattedMessage id="hello" values={{name}} />)',
+    '(<FormattedMessage id="hello" defaultMessage values={{name}} />)',
+    '(<FormattedMessage id="hello" defaultMessage="" values={{name}} />)',
+
+    'defineMessages()',
+    'defineMessages({key: {id: "greeting"}})',
+    'defineMessages({key: {id: "greeting", defaultMessage: ""}})',
+    'defineMessages({key: {id: "greeting", defaultMessage: a && b}})',
+  ],
+  invalid: [
+    {
+      code:
+        '(<FormattedMessage id="hello" defaultMessage="hello {" values={{name}} />)',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+    {
+      code:
+        '(<FormattedMessage id="hello" defaultMessage={"hello {"} values={{name}} />)',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+    {
+      code:
+        '(<FormattedMessage id="hello" defaultMessage={`hello {`} values={{name}} />)',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+    {
+      code: 'formatMessage({id: "greeting", defaultMessage: "hello {"})',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+
+    {
+      code: 'formatMessage({id: "greeting", defaultMessage: `hello {`})',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+    {
+      code:
+        'defineMessages({key: {id: "greeting", defaultMessage: "hello {"}})',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+    {
+      code:
+        'defineMessages({key: {id: "greeting", defaultMessage: `hello {`}})',
+      errors: ['defaultMessage should be a valid ICU MessageFormat string'],
+    },
+  ],
+})
